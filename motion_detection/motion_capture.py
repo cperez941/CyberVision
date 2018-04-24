187a6e9fcc063f6d4bad087d54107ba009d006ac
cperez941 <carlp941@gmail.com>
2018-04-24 18:38:16 -0500

    complete re-write of motion detection

--- motion_detection/motion_capture.py
+++ motion_detection/motion_capture.py
@@ -1,64 +1,77 @@
-# Author : Dave Jones (with minor edits by Carl Perez)
-# Date : 22 Feb 2018
-# Title : Motion Capture
-# Description : Takes video stream, and when the vectors between
-# multiple images reach a specified thresold, hold past 5 seconds of recording
-# record for 30 second and save the raw video file in the working directory.
-# NOTE : MUST BE EXECUTED ON A RASPBERRY PI
-
-from __future__ import division
-
+import io
 import picamera
-import numpy as np
-import time
+import motion_detect
+import ConfigParser
+import io
+from PIL import Image
 
-# Set up motion data
-motion_dtype = np.dtype([
-    ('x', 'i1'),
-    ('y', 'i1'),
-    ('sad', 'u2'),
-])
+prior_image = None
 
-# make params of video recording/motion detection easier to manipulate to
-# enable quicker calibration
-# TO DO
-# Enable automated calibration
-# Incorporate SAD
-camera_settings = {'width': 640, 'height': 480,'fps': 30, 'record_time': 5}
-motion_sensitivity = {'max number of vectors': 17, 'max magnitude of vectors': 86}
 
-# create motion detector class
-class my_motion_detector(object):
+config = ConfigParser.RawConfigParser(allow_no_value=True)
+config.readfp(io.BytesIO(sample_config))
 
-    def __init__(self, camera):
-        width, height = camera.resolution
-        self.cols = (width + 15) // 16
-        self.cols += 1  # there's always an extra column
-        self.rows = (height + 15) // 16
+def detect_motion(camera):
+    global prior_image
+    stream = io.BytesIO()
+    camera.capture(stream, format='jpeg', use_video_port=True)
+    stream.seek(0)
+    if prior_image is None:
+        prior_image = Image.open(stream)
+        return False
+    current_image = Image.open(stream)
+    # Compare current_image to prior_image to detect motion. This is left
+    # as an exercise for the reader!
+    is_there_motion = motion_detect.detect(
+            current_image,
+            prior_image,
+            0,
+            0.0002
+        )
+    # Once motion detection is done, make the current image the prior one
+    prior_image = current_image
+    return is_there_motion
 
-    # Load the motion data from the string to a numpy array
-    # Then, apply vectors between differences of images
-    # If there's >15 vectors with >80 magnitude, then there's motion
-    def write(self, s):
-        data = np.fromstring(s, dtype=motion_dtype)
-        data = data.reshape((self.rows, self.cols))
-        data = np.sqrt(
-        np.square(data['x'].astype(np.float)) +np.square(data['y'].astype(np.float))
-        ).clip(0, 255).astype(np.uint8)
-        if (data > motion_sensitivity['max magnitude of vectors']).sum() > motion_sensitivity['max number of vectors']:
-            print("Motion detected at " + time.strftime("%Y-%m-%d-%H:%M:%S") + "!")
-        return len(s)
+def write_before(stream):
+    # Write the entire content of the circular buffer to disk. No need to lock
+    # the stream here as we're definitely not writing to it simultaneously
+    with io.open('before.h264', 'wb') as output:
+        for frame in stream.frames:
+            if frame.header:
+                stream.seek(frame.position)
+                break
+        while True:
+            buf = stream.read1()
+            if not buf:
+                break
+            output.write(buf)
+    # Wipe the circular stream once we're done
+    stream.seek(0)
+    stream.truncate()
 
-# Record motion data to our custom output object
-# This will record upon the first instance of motion
-# i.e. Multiple instances of motion will not reset the video stream
-# Don't be alarmed by multiple print messages
-with picamera.PiCamera() as camera:
-    camera.resolution = (camera_settings['width'], camera_settings['height'])
-    camera.framerate = camera_settings['fps']
-    camera.start_recording(
-    './motion_detected.h264', format='h264',
-    motion_output=my_motion_detector(camera)
-    )
-    camera.wait_recording(camera_settings['record_time'])
-    camera.stop_recording()
+def process_motion(camera):
+    stream = picamera.PiCameraCircularIO(camera, seconds=config['recording_settings']['margin'])
+    camera.start_recording(stream, format='h264')
+    try:
+        while True:
+            camera.wait_recording(5)
+            if detect_motion(camera):
+                # As soon as we detect motion, split the recording to record
+                # the bits "after" motion
+                camera.split_recording('after.h264')
+                # While that's going, write the 10 seconds "before" motion to
+                # disk as well, and wipe the circular stream
+                write_video(stream)
+                # Wait until motion is no longer detected, then split recording
+                # back to the in-memory circular buffer
+                while detect_motion(camera):
+                    camera.wait_recording(5)
+                camera.split_recording(stream)
+    finally:
+        camera.stop_recording()
+
+
+if __name__ == '__main__':
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        process_motion(camera)
